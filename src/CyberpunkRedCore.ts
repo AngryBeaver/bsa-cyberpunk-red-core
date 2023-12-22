ac33f6cb8911373f57b50575c13f236e6b881d85
AngryBeaver <github@verplanmich.net>
2023-12-22 19:12:00 +0100

    initial commit

---
+++ src/CyberpunkRedCore.ts
@@ -0,0 +1,119 @@
+// @ts-ignore
+import CPRChat from "../../../systems/cyberpunk-red-core/modules/chat/cpr-chat.js?12";
+
+export class CyberpunkRedCore implements SystemApi {
+
+    skillList:SkillConfig[] = [];
+
+    get version() {
+        return 2;
+    }
+
+    get id() {
+        return "cyberpunk-red-core";
+    }
+
+
+    async actorRollSkill(actor, skillId){
+        const pack = game["packs"].get("cyberpunk-red-core.internal_skills");
+        const item = await pack.getDocument(skillId);
+        if(!item){
+            ui.notifications?.warn("I do not know skill "+skillId);
+            return null;
+        }
+        const x = item.createRoll("skill",actor);
+        const doRoll = await x.handleRollDialog({},actor,item);
+        if(doRoll){
+            await x.roll();
+            // @ts-ignore
+            await CPRChat.RenderRollCard(x);
+            x._roll._total = x.resultTotal;
+            return x._roll;
+        }
+        return null
+    }
+
+    async actorRollAbility(actor, abilityId){
+        const x = actor.createRoll("stat",abilityId);
+        const doRoll = await x.handleRollDialog({},actor,null);
+        if(doRoll){
+            await x.roll();
+            // @ts-ignore
+            await CPRChat.RenderRollCard(x);
+            x._roll._total = x.resultTotal;
+            return x._roll;
+        }
+        return null
+    }
+
+    actorCurrenciesGet(actor):Currencies {
+        return {"wealth":actor["system"].wealth.value};
+    }
+
+    async actorCurrenciesStore(actor, currencies: Currencies): Promise<void> {
+        await actor.update({system: {wealth:{value:currencies.wealth}}});
+    }
+
+    actorSheetAddTab(sheet, html, actor, tabData:{ id: string, label: string, html: string }, tabBody:string):void {
+        const tabs = $(html).find('nav.navtabs-right[data-group="primary"]');
+        const tabItem = $('<div class="tab-pink-underlay"><a class="tab-label text-semi tab-indent" data-tab="' + tabData.id + '" title="' + tabData.label + '">'+tabData.label+'</a></div>');
+        tabs.append(tabItem);
+        const body = $(html).find(".right-content-section");
+        const tabContent = $('<div class="tab flexcol" style="width:inherit" data-group="primary" data-tab="' + tabData.id + '"></div>');
+        body.append(tabContent);
+        tabContent.append(tabBody);
+    }
+
+    itemSheetReplaceContent(app, html, element):void {
+        html.find('.item-bottom-tabs-section').remove();
+        const sheetBody = html.find('.item-bottom-content-section');
+        sheetBody.addClass("flexrow");
+        sheetBody.empty();
+        sheetBody.append(element);
+    }
+
+    get configSkills():SkillConfig[] {
+        if(this.skillList.length === 0){
+            this.skillList = game["packs"].get("cyberpunk-red-core.internal_skills").index.map(skill=>{
+                return {id:skill._id,label:skill.name}
+            })
+        }
+        return this.skillList;
+    }
+
+    get configAbilities():AbilityConfig[] {
+        return Object.entries(game["i18n"].translations.CPR.global.stats).map(stat=>{
+            return {
+                id: stat[0],
+                label: stat[1] as string
+            }
+        })
+    }
+
+    get configCurrencies():CurrencyConfig[] {
+        return [
+            {
+                id: "wealth",
+                factor: 1,
+                label: "Eurobucks",
+            }
+        ]
+    }
+
+    get configCanRollAbility():boolean {
+        return true;
+    }
+    get configLootItemType(): string {
+        return "gear";
+    }
+
+    get itemPriceAttribute(): string {
+        return "system.price";
+    }
+
+    get itemQuantityAttribute(): string {
+        return "system.amount";
+    }
+
+
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import {CyberpunkRedCore} from "./CyberpunkRedCore.js";
+
+Hooks.on("beavers-system-interface.init", async function(){
+    beaversSystemInterface.register(new CyberpunkRedCore());
+});
